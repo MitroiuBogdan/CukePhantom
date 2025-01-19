fd41cbeba28a72d2fa9080b823f7c69a06556bc2
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2025-01-19 21:58:31 +0200

    Fix

--- src/test/java/com/yllu/cucumber_pw/RunCucumberTest.java
+++ src/test/java/com/yllu/cucumber_pw/RunCucumberTest.java
@@ -5,11 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.yllu.cucumber_pw") // Update with your package
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.yllu.cucumber_pw")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, json:results-local/cucumber-report.json")
 public class RunCucumberTest {
 }
