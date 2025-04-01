3bf4b2bb270b41bd0ed5931791c0759aade54ce4
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2025-04-02 01:10:53 +0300

    Add phantom bridge

---
+++ phantom-bridge/src/main/java/com/phbr/phantombridge/PhantomBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.phbr.phantombridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhantomBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhantomBridgeApplication.class, args);
+	}
+
+}
---
+++ phantom-bridge/src/test/java/com/phbr/phantombridge/PhantomBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phbr.phantombridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhantomBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
